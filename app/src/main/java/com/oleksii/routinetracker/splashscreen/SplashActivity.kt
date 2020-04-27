082a8ed7739ad6d6c00dc098f9195cc4c53b1d23
Oleksii <oleksii.m02@gmail.com>
2020-04-27 21:18:55 +0300

    Animations added

--- app/src/main/java/com/oleksii/routinetracker/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/oleksii/routinetracker/splashscreen/SplashActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import com.oleksii.routinetracker.MainActivity
 import com.oleksii.routinetracker.R
 
@@ -14,6 +17,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
+
+        AnimationUtils.loadAnimation(this, R.anim.app_name_animation).also {
+            findViewById<TextView>(R.id.app_name_text).startAnimation(it)
+        }
+        AnimationUtils.loadAnimation(this, R.anim.logo_animation).also {
+            findViewById<ImageView>(R.id.logo).startAnimation(it)
+        }
 
         val intent = Intent(this, MainActivity::class.java)
 
